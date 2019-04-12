fcf3baba456f7f857d172812e971048a170138c9
Cal Young <cyoung1024@gmail.com>
2019-04-12 22:00:13 +0200

    dexcom-share-client-swift

--- ShareClientUI/ShareService+UI.swift
+++ ShareClientUI/ShareService+UI.swift
@@ -30,6 +30,8 @@ extension ShareService: ServiceAuthenticationUI {
                      value: KnownShareServers.US.rawValue),
                     (title: LocalizedString("Outside US", comment: "Outside US share server option title"),
                      value: KnownShareServers.NON_US.rawValue)
+                    (title: LocalizedString("Nightscout", comment: "Custom Nightscout share server option title"),
+                     value: KnownShareServers.CUSTOM.rawValue)
 
                 ]
             )
