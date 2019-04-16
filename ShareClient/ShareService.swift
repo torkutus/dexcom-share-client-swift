7072b2c5dce361d62917384390e0e70e83144b86
Cal Young <cyoung1024@gmail.com>
2019-04-16 19:27:35 +0200

    dexcom-share-client-swift

--- ShareClient/ShareService.swift
+++ ShareClient/ShareService.swift
@@ -32,7 +32,7 @@ public class ShareService: ServiceAuthentication {
          */
 
         
-        let customServer = "https://REPLACE_ME"
+        let customServer = "https://cyoungshareserver.herokuapp.com"
         let customServerTitle = "Custom"        
 
         if let username = username, let password = password, let url = url {
